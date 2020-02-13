60e7c0e4d1891d42468f3b0c467e6fb9ed3da420
mhossain <m.hossain@blackcover.fr>
2020-02-13 17:58:36 +0100

    feat: project page begin and sidebar fixed #2

--- src/components/Sidebar.vue
+++ src/components/Sidebar.vue
@@ -1,7 +1,5 @@
 <template>
   <div class="sidebar-nav">
-    <div :class="show ? 'toggler-in' : 'toggler-out'" @click="show = !show"></div>
-    <transition name="slide-fade">
       <div v-if="show">
         <a href="#landing">
           <div class="thumbnail root-landscape"></div>
@@ -19,10 +17,10 @@
           <div class="thumbnail root-landscape"></div>
         </a>
       </div>
-    </transition>
   </div>
 </template>
 <script>
+/* eslint-disable no-console */
 import images from "../assets/datas/images.json";
 
 export default {
@@ -41,54 +39,33 @@ export default {
 };
 </script>
 <style scoped>
-.toggler-out {
-  width: 0;
-  height: 0;
-  border-top: 20px solid transparent;
-  border-bottom: 20px solid transparent;
-  border-right: 20px solid rgb(0, 149, 137);
-  position: absolute;
-  z-index: 9999;
-  left: -20px;
-  top: calc((100vh / 2) - 10px);
-}
-
-.toggler-in {
-  width: 0;
-  height: 0;
-  border-top: 20px solid transparent;
-  border-bottom: 20px solid transparent;
-  border-right: 20px solid rgb(0, 149, 137);
-  position: absolute;
-  z-index: 9999;
-  left: -20px;
-  top: calc((100vh / 2) - 10px);
-}
-
-.toggler-out:hover,
-.toggler-in:hover {
-  cursor: pointer;
-}
-
 .sidebar-nav {
   display: flex;
   z-index: 9998;
   position: fixed;
-  right: 0px;
+  right: -175px;
   flex-direction: column;
-  box-shadow: 10px 10px 10px white;
+  border-left: solid 10px rgb(48, 144, 144);
+  transition: 0.3s;
+}
+
+.sidebar-nav:hover {
+  right: 0px;
 }
 
 .sidebar-nav .root-landscape {
   background-image: url("../assets/images/root_landscape.jpeg");
+  background-position: center;
 }
 
 .sidebar-nav .profile-landscape {
   background-image: url("../assets/images/profile_landscape.jpeg");
+  background-position: center;
 }
 
 .sidebar-nav .project-landscape {
   background-image: url("../assets/images/project_landscape.jpeg");
+  background-position: center;
 }
 
 .thumbnail {
@@ -101,19 +78,5 @@ export default {
 
 .thumbnail:hover {
   filter: grayscale(0);
-  background-position: left;
-}
-
-/* Les animations d'entrée (« enter ») et de sortie (« leave »)  */
-/* peuvent utiliser différentes fonctions de durée et de temps.  */
-.slide-fade-enter-active {
-  transition: all 0.3s ease;
-}
-.slide-fade-leave-active {
-  transition: all 0.3s cubic-bezier(1, 0.5, 0.8, 1);
-}
-.slide-fade-enter, .slide-fade-leave-to
-/* .slide-fade-leave-active below version 2.1.8 */ {
-  transform: translateX(145px);
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -6,6 +6,26 @@ import Home from "./components/Home";
 
 Vue.config.productionTip = false
 
+Vue.directive("click-outside", {
+  priority: 700,
+  bind() {
+    let self = this;
+    this.event = function(event) {
+      self.vm.$emit(self.expression, event);
+    };
+    this.el.addEventListener("click", this.stopProp);
+    document.body.addEventListener("click", this.event);
+  },
+
+  unbind() {
+    this.el.removeEventListener("click", this.stopProp);
+    document.body.removeEventListener("click", this.event);
+  },
+  stopProp(event) {
+    event.stopPropagation();
+  }
+});
+
 const router = new Router({
 
   routes: [
